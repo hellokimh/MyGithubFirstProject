1e87df07f718ca0bb2d18c6909adcd31b7aeaaff
hellokimh <kimh1992@sina.com>
2020-12-14 01:33:32 +0800

    新增文件

--- app/src/main/java/com/example/mygithubfirstproject/MainActivity.java
+++ app/src/main/java/com/example/mygithubfirstproject/MainActivity.java
@@ -2,9 +2,13 @@ package com.example.mygithubfirstproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,7 +17,17 @@ public class MainActivity extends AppCompatActivity {
         initView();
     }
 
+    /**
+     * 初始化控件 测试GitHub 用git
+     */
     private void initView() {
+        Button btn = findViewById(R.id.btn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, TestActivity.class));
+            }
+        });
 
     }
 }

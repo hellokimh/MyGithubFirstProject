5847220a299427055de6afab61968f5f9af94c1f
hellokimh <kimh1992@sina.com>
2020-12-13 16:59:02 +0800

    新增文件

--- app/src/main/java/com/example/mygithubfirstproject/TestActivity.java
+++ app/src/main/java/com/example/mygithubfirstproject/TestActivity.java
@@ -3,6 +3,7 @@ package com.example.mygithubfirstproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class TestActivity extends AppCompatActivity {
 
